7f5def7043c97822bfdddd32fad1cd287515d973
Minh Q. Le <77988736+lqm181@users.noreply.github.com>
2024-04-15 18:47:01 -0400

    Connected signup with backend.

--- frontend/src/app/signup.tsx
+++ frontend/src/app/signup.tsx
@@ -6,14 +6,72 @@ import {
   Platform,
   ScrollView,
 } from 'react-native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Button, Input } from '../components';
-import { COLORS, FONTWEIGHT, SIZES } from '../constants';
+import { BACKEND_API_URL, COLORS, FONTWEIGHT, SIZES } from '../constants';
 import { Link } from 'expo-router';
 import { FontAwesome6 } from '@expo/vector-icons';
+import useDataFetcher from '../hooks/useDataFetcher';
 
 const signup = () => {
+  const [input, setInput] = useState<{
+    firstName: string | undefined;
+    lastName: string | undefined;
+    email: string | undefined;
+    password: string | undefined;
+    confirmedPassword: string | undefined;
+  }>({
+    firstName: undefined,
+    lastName: undefined,
+    email: undefined,
+    password: undefined,
+    confirmedPassword: undefined,
+  });
+
+  const { isLoading, error, fetchData } = useDataFetcher();
+  const [registerError, setRegisterError] = useState<null | Error>();
+
+  // Todo: Add form validation.
+  const handleSignup = async () => {
+    if (
+      !input.firstName ||
+      !input.lastName ||
+      !input.email ||
+      !input.password ||
+      !input.confirmedPassword
+    ) {
+      setRegisterError(new Error('Please enter all information.'));
+      return;
+    } else if (input.password !== input.confirmedPassword) {
+      setRegisterError(new Error('Passwords do not match.'));
+      return;
+    }
+
+    const userData = await fetchData(`${BACKEND_API_URL}/api/auth/register`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: {
+        ...input,
+      },
+    });
+
+    if (userData) {
+      // TODO: Add redirection and update user context.
+      setRegisterError(null);
+    }
+  };
+
+  useEffect(() => {
+    if (error) {
+      setRegisterError(
+        new Error('Email already exists. Please use a different email address.')
+      );
+    }
+  }, [error]);
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
       <KeyboardAvoidingView
@@ -34,6 +92,9 @@ const signup = () => {
               </Text> */}
             </View>
             <View style={styles.inputContainer}>
+              {registerError && (
+                <Text style={styles.errorText}>* {registerError.message}</Text>
+              )}
               <View style={styles.inputRow}>
                 <FontAwesome6 name='user-large' size={20} color='black' />
                 <Input
@@ -42,6 +103,10 @@ const signup = () => {
                   placeholder='First Name'
                   containerStyle={[styles.input]}
                   style={{ borderWidth: 1 }}
+                  value={input?.firstName}
+                  onChangeText={(text) =>
+                    setInput((prev) => ({ ...prev, firstName: text }))
+                  }
                 />
                 <Input
                   variant='outlined'
@@ -49,6 +114,10 @@ const signup = () => {
                   placeholder='Last Name'
                   containerStyle={[styles.input]}
                   style={{ borderWidth: 1 }}
+                  value={input?.lastName}
+                  onChangeText={(text) =>
+                    setInput((prev) => ({ ...prev, lastName: text }))
+                  }
                 />
               </View>
               <View style={styles.inputRow}>
@@ -60,6 +129,10 @@ const signup = () => {
                   containerStyle={[styles.input]}
                   inputMode='email'
                   style={{ borderWidth: 1 }}
+                  value={input?.email}
+                  onChangeText={(text) =>
+                    setInput((prev) => ({ ...prev, email: text }))
+                  }
                 />
               </View>
               <View style={styles.inputRow}>
@@ -71,6 +144,10 @@ const signup = () => {
                   secureTextEntry={true}
                   textContentType='newPassword'
                   containerStyle={[styles.input]}
+                  value={input?.password}
+                  onChangeText={(text) =>
+                    setInput((prev) => ({ ...prev, password: text }))
+                  }
                 />
               </View>
               <View style={styles.inputRow}>
@@ -82,11 +159,21 @@ const signup = () => {
                   secureTextEntry={true}
                   textContentType='newPassword'
                   containerStyle={[styles.input]}
+                  value={input?.confirmedPassword}
+                  onChangeText={(text) =>
+                    setInput((prev) => ({ ...prev, confirmedPassword: text }))
+                  }
                 />
               </View>
             </View>
             <View style={styles.actionContainer}>
-              <Button style={styles.button}>Sign up</Button>
+              <Button
+                style={styles.button}
+                disabled={isLoading}
+                onPress={handleSignup}
+              >
+                {isLoading ? 'Signing up...' : 'Sign up'}
+              </Button>
               <Text>
                 Already have an account?{' '}
                 <Link href='/login' style={styles.link}>
@@ -135,6 +222,7 @@ const styles = StyleSheet.create({
     flex: 1,
     height: '100%',
   },
+  errorText: { color: COLORS.error, marginBottom: 8 },
   actionContainer: {
     display: 'flex',
     justifyContent: 'center',
